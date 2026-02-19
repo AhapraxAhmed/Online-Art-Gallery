0f295ed836db3a88b227cecf39cba7c9b3178f0b
AHMED <aptech356@gmail.com>
2026-02-20 00:25:02 +0500

    first commit with whole project

--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ddee1e431871f0c991a848c00d44c4be24ee03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bace674cf143fe733c84bad25549e5aa39a4a10e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
